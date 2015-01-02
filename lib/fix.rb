772e05230cb486a6a7ddd2de768b60f678d26878
Victor Mours <victor.mours@tigerlilyapps.com>
2015-01-02 15:55:59 +0100

    fix

--- lib/fix.rb
+++ lib/fix.rb
@@ -3,6 +3,7 @@ Pry::Commands.create_command "fix" do
   def process
     current_class = target.eval('self.class')
     if current_class.ancestors.include?(RSpec::Core::ExampleGroup)
+      described_method = current_class.metadata[:full_description]
       run "edit #{described_method}"
     else
       run "edit -c"
