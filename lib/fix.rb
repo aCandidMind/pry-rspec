87ee348287e7afa3e3799e04b47c6f151aff6420
Victor Mours <victor.mours@tigerlilyapps.com>
2015-01-02 15:49:01 +0100

    Edit current file if failure outside of specs

--- lib/fix.rb
+++ lib/fix.rb
@@ -1,8 +1,12 @@
 Pry::Commands.create_command "fix" do
 
   def process
-    described_method = target.eval('self.class.metadata[:full_description]')
-    run "edit #{described_method}"
+    current_class = target.eval('self.class')
+    if current_class.ancestors.include?(RSpec::Core::ExampleGroup)
+      run "edit #{described_method}"
+    else
+      run "edit -c"
+    end
     run "try-again"
   end
 end
